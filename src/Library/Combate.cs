c1d84e401953678aee484e000171714507b9f710
Rodrigofagundez2004 <yeekko19@gmail.com>
2024-05-07 14:39:41 -0300

    test

--- src/Library/Combate.cs
+++ src/Library/Combate.cs
@@ -7,16 +7,26 @@ public class Combate
     public void atacar (Personaje atacante, Personaje defensor)
     {
         int daño = atacante.AtaqueTotal() - defensor.DefensaTotal();
+        if (defensor.vida>0){
         if (daño > 0)
         {
             defensor.vida -= daño;
-        }
+        }}
         Console.WriteLine($"{atacante.GetNombre()} ataca a {defensor.GetNombre()} por {daño} puntos de daño.");
 
     }
     public void curar (Personaje personaje)
     {
-        personaje.vida = 100;
-        
+
+        if (personaje.vida == 0){
+            Console.WriteLine("Su personaje esta muerto");
+        }
+        if (personaje.vida>100){
+            Console.WriteLine("su personaje esta al maximo de vida, no es posible curarlo.");
+            }
+        else {
+             personaje.vida = personaje.ataque/2;
+             Console.WriteLine($"ALa Vida de su personaje es {personaje.vida}.");
+             }
     }
 }
