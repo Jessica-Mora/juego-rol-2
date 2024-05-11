7e1218508739da5334ce2daa8ecc1c18d738f84a
Rodrigofagundez2004 <yeekko19@gmail.com>
2024-05-11 16:08:21 -0300

    test

--- src/Library/Elfo.cs
+++ src/Library/Elfo.cs
@@ -1,16 +1,55 @@
 using System.Collections.Generic;
-
 public class Elfo : Personaje
 {
-    
-    public Elfo (string nombre, int vida, int ataque, int defensa): base (nombre, vida, ataque, defensa){
-    
-    }
+   private List<Elemento> equipamiento;
 
+   public Elfo(string nombre, int vida, int ataque, int defensa) : base(nombre, vida, ataque, defensa)
+   {
+       equipamiento = new List<Elemento>();
+   }
 
+   public void EquiparArma(Arma arma)
+   {
+       equipamiento.Add(arma);
+       AtaqueTotal += arma.ValorAtaque;
+   }
 
+   public void EquiparDefensa(Defensa defensa)
+   {
+       equipamiento.Add(defensa);
+       DefensaTotal += defensa.ValorDefensa;
+   }
 
+   public void EquiparArmaDefensa(ArmaDefensa armaDefensa)
+   {
+       equipamiento.Add(armaDefensa);
+       AtaqueTotal += armaDefensa.ValorAtaque;
+       DefensaTotal += armaDefensa.ValorDefensa;
+   }
 
+   public override int AtaqueTotal
+   {
+       get
+       {
+           int ataqueTotal = base.AtaqueTotal;
+           foreach (Elemento elemento in equipamiento)
+           {
+               ataqueTotal += elemento.ValorAtaque;
+           }
+           return ataqueTotal;
+       }
+   }
 
+   public override int DefensaTotal
+   {
+       get
+       {
+           int defensaTotal = base.DefensaTotal;
+           foreach (Elemento elemento in equipamiento)
+           {
+               defensaTotal += elemento.ValorDefensa;
+           }
+           return defensaTotal;
+       }
+   }
 }
-
